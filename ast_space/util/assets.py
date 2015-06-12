31fff7088cc9e26e1adad1798755a2f2e45bcee3
Fandi Peng <fandi@gatech.edu>
2015-06-12 08:51:01 +0800

    update

--- ast_space/util/assets.py
+++ ast_space/util/assets.py
@@ -1,19 +1,31 @@
 from flask.ext.assets import Bundle, Environment
 from .. import app
 
-bundles = {
 
-    'base_js': Bundle(
-        'js/home.js',
-        output='gen/home.js'),
+# a way to organize static files
+bundles = {
 
     'base_css': Bundle(
         'css/lib/foundation.css',
+        'css/lib/normalize.css',
         'css/home.css',
-        output='gen/home.css')
+        filters='cssmin',
+        output='gen/base.css'),
 
+    'base_js': Bundle(
+        'js/home.js',
+        'js/lib/vendor/modernizr.js',
+        filters='jsmin',
+        output='gen/base.js',
+        ),
+
+    'bottom_js': Bundle(
+        'js/lib/vendor/jquery.js',
+        'js/lib/foundation.min.js',
+        filters='jsmin',
+        output='gen/bottom.js',
+        )
 }
 
 assets = Environment(app)
-
 assets.register(bundles)
